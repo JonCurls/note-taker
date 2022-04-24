e661bc79013e1ed9122cf1bb4bf9b021edcbb31a
MrCurls <JonCurls@gmail.com>
2022-04-24 11:44:01 -0500

    fiddling

--- README.md
+++ README.md
@@ -1,1 +1,61 @@
-# Note Taker Starter Code
+[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)
+
+# **Server Note Taker**
+
+## **Thumbnail**
+
+![ScreenShot](./assets/thumbnail.PNG)
+
+## **Description**
+
+- An application that can write and save notes from a JSON file.
+
+## **Built With**
+
+- HTML
+- CSS
+- JAVASCRIPT
+- EXPRESS.JS
+
+## **Table of Contents**
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Testing](#testing)
+- [Questions](#questions)
+- [Deployment](#deployment)
+
+## **Installation**
+
+- dl from Github
+
+## **Usage**
+
+- run by: node server.js
+
+## **License**
+
+- ISC
+
+## **Contributing**
+
+How to Contrubute
+
+- Open source baby
+
+## **Testing**
+
+- TBD
+
+## **Questions**
+
+Made with ❤️💧🩸
+
+- www.github.com/JonCurls
+- JonCurls@gmail.com
+
+## **Deployment**
+
+https://ancient-badlands-45876.herokuapp.com/
--- server.js
+++ server.js
@@ -22,14 +22,15 @@ app.get("/api/notes", (req, res) => {
 
 // When user adds a new Task
 app.post("/api/notes", (req, res) => {
-  let newNote = req.body;
-  newNote.id = uuidv4();
-  res.sendFile(path.join(__dirname, "./db/db.json"));
-  fs.readFile("./db/db.json", "utf-8", (err, data) => {
+  //   console.log(newNote);
+  //   res.sendFile(path.join(__dirname, "./db/db.json"));
+  fs.readFile("./db/db.json", "utf8", (err, data) => {
     if (err) throw err;
+    let newNote = req.body;
+    newNote.id = uuidv4();
     let newTask = JSON.parse(data);
     newTask.push(newNote);
-    fs.writeFile("./db/db.json", JSON.stringify(newTask), "utf-8", (err) => {
+    fs.writeFile("./db/db.json", JSON.stringify(newTask), "utf8", (err) => {
       if (err) throw err;
       console.log("successfully added!");
     });
@@ -38,7 +39,7 @@ app.post("/api/notes", (req, res) => {
 
 // Catch-all for non specific pages
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "/public/index.html"));
+  res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
 app.listen(PORT, () => {
