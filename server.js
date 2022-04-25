39c862f0ba0a7514fa7b84ef68c52de4b6e035ae
MrCurls <JonCurls@gmail.com>
2022-04-24 21:11:57 -0500

    final cleanup and comments

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// import the blah blahs
 const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
@@ -11,30 +12,33 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
+// user GET request to the Notes page
 app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
-// When user access notes page
+// user GET request for the JSON db
 app.get("/api/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "./db/db.json"));
   let results = notes;
 });
 
-// When user adds a new Task
+// user POST request to add new Task
 app.post("/api/notes", (req, res) => {
   fs.readFile("./db/db.json", "utf8", (err, data) => {
+    // reads the users input
     if (err) throw err;
     let newNote = req.body;
-    newNote.id = uuidv4();
+    newNote.id = uuidv4(); // create unique ID
     let newTask = JSON.parse(data);
     newTask.push(newNote);
     fs.writeFile("./db/db.json", JSON.stringify(newTask), "utf8", (err) => {
+      // writes to db
       if (err) throw err;
       console.log("successfully added!");
+      res.json(newNote);
     });
   });
-  res.sendFile(path.join(__dirname, "./db/db.json"));
 });
 
 // Catch-all for non specific pages
@@ -42,6 +46,7 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
+// page listener
 app.listen(PORT, () => {
   console.log(`api server now on port ${PORT}!`);
 });
